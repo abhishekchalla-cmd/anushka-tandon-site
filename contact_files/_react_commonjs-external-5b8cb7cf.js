ccab2fbaaaa2bf385aa4b672ecc76ea5d7b9f573
Abhishek Challa <abhishek.challa.97@gmail.com>
2021-02-20 05:12:24 +0530

    init

---
+++ contact_files/_react_commonjs-external-5b8cb7cf.js
@@ -0,0 +1,2 @@
+define("@wsb/guac-widget-shared/c/_react_commonjs-external-5b8cb7cf.js",["exports"],(function(e){"use strict";const c=global.React||guac.react;e._global__global_React____guac_react_=c})),"undefined"!=typeof window&&(window.global=window);
+//# sourceMappingURL=_react_commonjs-external-5b8cb7cf.js.map
